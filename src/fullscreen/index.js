38f1b549a92844aa5b0009e6a1472d369da0ed7b
Matt Baker <baker.mattc@gmail.com>
2016-05-24 14:37:15 -0600

    Add ambience

--- src/fullscreen/index.js
+++ src/fullscreen/index.js
@@ -6,6 +6,7 @@ const configureBtn = document.getElementById('configure')
 const currentEl = document.getElementById('current')
 const nextEl = document.getElementById('next')
 const countEl = document.getElementById('count')
+const ambience = document.getElementById('fullscreenAudio')
 
 function lpad(val) {
   return val < 10
@@ -40,3 +41,5 @@ startTurnBtn.addEventListener('click', _ => ipc.send('startTurn'))
 configureBtn.addEventListener('click', _ => ipc.send('configure'))
 
 ipc.send('fullscreenWindowReady')
+
+ambience.play();
