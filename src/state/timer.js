802e03fc6b943f919d5d413edd5120565c80911a
Allan Stewart <allan-stewart@pluralsight.com>
2016-06-28 23:47:38 -0600

    Create new timer class

---
+++ src/state/timer.js
@@ -0,0 +1,30 @@
+class Timer {
+  constructor(options, callback) {
+    this.rateMilliseconds = options.rateMilliseconds || 1000
+    this.time = options.time || 0
+    this.change = options.countDown ? -1 : 1
+    this.callback = callback
+  }
+
+  start() {
+    if (!this.interval) {
+      this.interval = setInterval(() => {
+        this.time += this.change;
+        this.callback(this.time)
+      }, this.rateMilliseconds)
+    }
+  }
+
+  pause() {
+    if (this.interval) {
+      clearInterval(this.interval)
+      this.interval = null
+    }
+  }
+
+  reset(value) {
+    this.time = value
+  }
+}
+
+module.exports = Timer
---
+++ test/state/timer.specs.js
@@ -0,0 +1,105 @@
+let Timer = require('../../src/state/timer')
+let assert = require('assert')
+
+describe('Timer', () => {
+  let timer
+  let timerOptions
+  let callbacks
+
+  let createTimer = () => {
+    timer = new Timer(timerOptions, x => callbacks.push(x))
+  }
+
+  beforeEach(() => {
+    callbacks = []
+    timerOptions = { rateMilliseconds: 10, time: 50, countDown: true }
+    createTimer()
+  })
+
+  afterEach(() => {
+    timer.pause()
+  })
+
+  describe('on construction', () => {
+    describe('with specified options', () => {
+      it('should have the specified rateMilliseconds value', () => {
+        assert.equal(timer.rateMilliseconds, timerOptions.rateMilliseconds)
+      })
+
+      it('should have the specified value', () => {
+        assert.equal(timer.time, timerOptions.time)
+      })
+
+      it('should have a change value based on the specified countDown', () => {
+        assert.equal(timer.change, -1)
+      })
+    })
+
+    describe('with default options', () => {
+      beforeEach(() => {
+        timerOptions = {}
+        createTimer()
+      })
+
+      it('should have the default rateMilliseconds value', () => {
+        assert.equal(timer.rateMilliseconds, 1000)
+      })
+
+      it('should have the default time value', () => {
+        assert.equal(timer.time, 0)
+      })
+
+      it('should have the default change value', () => {
+        assert.equal(timer.change, 1)
+      })
+    })
+  })
+
+  describe('start', () => {
+    it('should generate callbacks when counting down', (done) => {
+      timer.start()
+      setTimeout(() => {
+        assert.equal(callbacks.join(','), '49,48')
+        done()
+      }, 25)
+    })
+
+    it('should generate callbacks when counting up', (done) => {
+      timerOptions.countDown = false
+      createTimer()
+
+      timer.start()
+      setTimeout(() => {
+        assert.equal(callbacks.join(','), '51,52')
+        done()
+      }, 25)
+    })
+  })
+
+  describe('pause', () => {
+    it('should stop further callbacks from occuring', (done) => {
+      timer.start()
+      setTimeout(() => timer.pause(), 25)
+      setTimeout(() => {
+        assert.equal(callbacks.join(','), '49,48')
+        done()
+      }, 50)
+    })
+  })
+
+  describe('reset', () => {
+    it('should set a new time value when the timer is not running', () => {
+      timer.reset(42)
+      assert.equal(timer.time, 42)
+    })
+
+    it('should set a new time value when the timer is running', (done) => {
+      timer.start()
+      setTimeout(() => timer.reset(20), 25)
+      setTimeout(() => {
+        assert.equal(callbacks.join(','), '49,48,19,18')
+        done()
+      }, 49)
+    })
+  })
+})
